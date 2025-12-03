53f617a581e557499f60538ccd1e47c17ad108a9
ihaimovich27-star <ihaimovich27@dtechhs.org>
2025-12-03 23:23:20 +0000

    commit

---
+++ test.c
@@ -0,0 +1,21 @@
+// const char *nouns[] = {"cat", "house", "water", "car", "tree"};
+// const char *verb[] = {"runs", "jumps", "sits down", "climbs", "talks"};
+// const char *adjective[] = {"little", "silly", "wise", "young", "tired"};
+// const char *adverb[] = {"quickly", "angrily", "happily"};
+// const char *determiner[] = {"the", "a", "my", "your", "his", "her"};
+// const char *preposition[] = {"over", "near", "towards", "in front of"};
+
+// int count = sizeof(nouns) / sizeof(nouns[0]);
+
+// srand(time(NULL));            // seed random generator
+// int index = rand() % count;   // pick a random index
+
+// printf("Random noun: %s\n", nouns[index]);
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+
